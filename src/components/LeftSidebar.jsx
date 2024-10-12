c50145442d0ca22280cbfc039cb95859013d1285
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-10-12 12:56:45 +0100

    Add authentication using Clerk

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,9 +1,16 @@
 import Link from 'next/link';
 import { FaXTwitter } from 'react-icons/fa6';
 import { HiHome } from 'react-icons/hi';
-
-export default function LeftSidebar() {
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from '@clerk/nextjs';
+// import MiniProfile from './MiniProfile';
+export default function Sidebar() {
   return (
+    <div className='flex flex-col p-3 justify-between h-screen items-center'>
       <div className='flex flex-col gap-4 p-3'>
         <Link href='/'>
           <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200 ' />
@@ -16,8 +23,17 @@ export default function LeftSidebar() {
           <span className='font-bold hidden xl:inline'>Home</span>
         </Link>
         <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
-          Sign In
+          <SignedIn>
+            <SignOutButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
         </button>
       </div>
+      <SignedIn>
+        {/* <MiniProfile /> */}
+      </SignedIn>
+    </div>
   );
 }
